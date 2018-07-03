58910bb29cd4d6910fc1903f582eb50093ccf400
Hieu <pmh.ars14@gmail.com>
2018-07-03 15:51:57 +0700

    correct api-key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: 'AIzaSyDIrM4D-Fbyc59eV0T39eXSOsIOLDz0w4U',
+  	apiKey: '10fbf3376a6b4b63aae14502dd65fbee',
     authDomain: 'my-angular-hieudauto.firebaseapp.com',
     databaseURL: 'https://my-angular-hieudauto.firebaseio.com',
     projectId: 'my-angular-hieudauto',
